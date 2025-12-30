1a4647453968ee93fecdb56aea6bff76c29bfb59
Kamyar Esmaeili Pourfarhangi <kamyar.esmaeili@gmail.com>
2025-12-30 15:06:20 -0500

    model and test

--- model.py
+++ model.py
@@ -70,6 +70,92 @@ class FeedForwardBlock(nn.Module):
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
     
 # Multi-head attention layer
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model:int, num_head: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.num_head = num_head
+        assert d_model % num_head == 0, "d_model is not divissble by num_heads"
+
+        self.d_k = d_model // num_head
+        self.w_q = nn.Linear(d_model, d_model) # wq
+        self.w_k = nn.Linear(d_model, d_model) # wk
+        self.w_v = nn.Linear(d_model, d_model) # wv
+        
+        self.w_o = nn.Linear(d_model, d_model) # wo
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        # calculating attention
+        # (Batch, num_h, seq_len, d_k) --> (Batch, num_h, seq_len, seq_len)
+        attention_scores = (query @ key.transpose(-2,-1)) / math.sqrt(d_k)
+        # applying mask before softmax
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim=-1) # (Batch, num_h, seq_len, seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        
+        return (attention_scores @ value), attention_scores
+
+
+
+    def forward(self, q, k, v, mask): # if we want some words not interacting with each other mask is used
+        query = self.w_q(q)  # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        key = self.q_k(k)    # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        value = self.q_v(v)   # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, num_h, d_k) --> (Batch, num_h, seq_len, d_k)
+        # transpose if done to make sure that each head sees the entire sentence but small parts of the embeddings
+        query = query.view(query.shape[0], query.shape[1], self.num_head, self.d_k).transpose(1,2)
+        key = key.view(key.shape[0], key.shape[1], self.num_head, self.d_k).transpose(1,2)
+        value = value.view(value.shape[0], value.shape[1], self.num_head, self.d_k).transpose(1,2)
+
+        x, self.attention_scores = MultiHeadAttention.attention(query, key, value, self.dropout)
+
+        # (Batch, num_head, seq_len, d_k) --> (Batch, seq_len, num_head, d_k) --> (Batch, seq_len, d_model)
+        # using contiguous so pytorch would do it in-place for memory management
+        x = x.transpose(1,2).contiguous().view(x.shape[0], -1, self.num_head * self.d_k)
+
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        return self.w_o(x)
+    
+class ResidualConnection(nn.Module):
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+    
+class EncoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttention, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connection = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.residual_connection[0](x, lambda x: self.self_attention_block(x,x,x,src_mask))
+        x = self.residual_connection[1](x, self.feed_forward_block)
+
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+
+
 
 
     
